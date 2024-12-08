a408823790ee3ff94b7c8eb4c4428d7258fc67a2
ibrahimdevop <ibrahimdevop21@gmail.com>
2024-12-08 21:50:13 +0200

    Add .nojekyll to disable jekyll / edit astro.config.mjs to output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
+import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static', // Ensure static output
+  base: '/', // Update base if deploying to a subfolder
   integrations: [tailwind()],
-  output: 'server',
   adapter: vercel(),
 });
