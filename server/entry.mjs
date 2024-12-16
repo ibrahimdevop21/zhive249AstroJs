0a971004db4a0ee84478a13b2d5c7cc207898f66
ibrahimdevop21 <ibrahimdevop21@users.noreply.github.com>
2024-12-16 02:13:18 +0000

    deploy: ab8c143042264b691da689d3e8fd04f1b0f70cf2

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DXymFsYP.mjs';
-import { manifest } from './manifest_CANLzIwb.mjs';
+import { manifest } from './manifest_PdYA8rYm.mjs';
 
 const serverIslandMap = new Map([
 ]);;
